7b1726bf7b29ce027beb82aff3f6846af4f36905
Dharmik Anghan <107908300+dharmik5291@users.noreply.github.com>
2023-02-22 15:53:20 +0530

    FIX: register page ui changes

--- lib/app/widgets/custom_text_field.dart
+++ lib/app/widgets/custom_text_field.dart
@@ -11,11 +11,12 @@ class CustomTextFormField extends StatelessWidget {
   final dynamic suffixIcon;
   final dynamic icon;
   final TextInputType? textInputType;
-  final int? maxleng;
+  final int? maxLength;
   final dynamic onTap;
   final dynamic focusNode;
   final bool? readonly;
   final bool? errorBorder;
+  final String? label;
 
   const CustomTextFormField({
     Key? key,
@@ -31,53 +32,62 @@ class CustomTextFormField extends StatelessWidget {
     this.obscureText = false,
     this.icon,
     this.textInputType,
-    this.maxleng,
+    this.maxLength,
     this.readonly = false,
+    this.label,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      controller: controller,
-      textInputAction: TextInputAction.next,
-      readOnly: readonly!,
-      obscureText: obscureText!,
-      focusNode: focusNode,
-      validator: validator,
-      onChanged: onChanged,
-      onTap: onTap,
-      cursorColor: AppColors.black,
-      decoration: InputDecoration(
-        counterText: "",
-        fillColor: AppColors.fillColor,
-        filled: true,
-        prefixIcon: icon,
-        focusColor: AppColors.black,
-        suffixIcon: suffixIcon,
-        suffixIconColor: AppColors.black,
-        border: InputBorder.none,
-        enabledBorder: OutlineInputBorder(
-          borderSide: BorderSide(width: 1, color: AppColors.transparent),
-          borderRadius: BorderRadius.circular(15),
+    return Padding(
+      padding: hasPadding == true
+          ? const EdgeInsets.symmetric(horizontal: 16)
+          : EdgeInsets.zero,
+      child: TextFormField(
+        controller: controller,
+        textInputAction: TextInputAction.next,
+        readOnly: readonly!,
+        obscureText: obscureText!,
+        focusNode: focusNode,
+        validator: validator,
+        onChanged: onChanged,
+        onTap: onTap,
+        cursorColor: AppColors.black,
+        decoration: InputDecoration(
+          counterText: "",
+          fillColor: AppColors.fillColor,
+          labelText: label,
+          labelStyle: TextStyle(color: AppColors.black),
+          filled: true,
+          prefixIcon: icon,
+          focusColor: AppColors.black,
+          suffixIcon: suffixIcon,
+          suffixIconColor: AppColors.black,
+          border: InputBorder.none,
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(width: 1, color: AppColors.transparent),
+            borderRadius: BorderRadius.circular(15),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(width: 1, color: AppColors.black),
+            borderRadius: BorderRadius.circular(15),
+          ),
+          errorBorder: OutlineInputBorder(
+            borderSide: BorderSide(width: 1, color: AppColors.black),
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+          focusedErrorBorder: OutlineInputBorder(
+            borderSide: BorderSide(width: 1, color: AppColors.black),
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+          hintText: hintText,
+          hintStyle: TextStyle(color: AppColors.black),
         ),
-        focusedBorder: OutlineInputBorder(
-          borderSide: BorderSide(width: 1, color: AppColors.black),
-          borderRadius: BorderRadius.circular(15),
-        ),
-        errorBorder: OutlineInputBorder(
-          borderSide: BorderSide(width: 1, color: AppColors.black),
-          borderRadius: BorderRadius.circular(8.0),
-        ),
-        focusedErrorBorder: OutlineInputBorder(
-          borderSide: BorderSide(width: 1, color: AppColors.black),
-          borderRadius: BorderRadius.circular(8.0),
-        ),
-        hintText: hintText,
-        hintStyle: TextStyle(color: AppColors.black),
+        style:
+            TextStyle(color: AppColors.black, decoration: TextDecoration.none),
+        keyboardType: textInputType,
+        maxLength: maxLength,
       ),
-      style: TextStyle(color: AppColors.black, decoration: TextDecoration.none),
-      keyboardType: textInputType,
-      maxLength: maxleng,
     );
   }
 }
