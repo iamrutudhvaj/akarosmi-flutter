f22f70b18659f3bfe5ee531404f85a092e4800ab
skpatel9081 <krunals9102001@gmail.com>
2023-02-22 12:31:49 +0530

    WIP: book list

--- lib/app/modules/books_page/controllers/books_page_controller.dart
+++ lib/app/modules/books_page/controllers/books_page_controller.dart
@@ -1,3 +1,20 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class BooksPageController extends GetxController {}
+class BooksPageController extends GetxController
+    with GetTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void onInit() {
+    _controller = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 200));
+    super.onInit();
+  }
+
+  @override
+  dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+}
--- lib/app/modules/books_page/views/books_page_view.dart
+++ lib/app/modules/books_page/views/books_page_view.dart
@@ -35,11 +35,118 @@ class BooksPageView extends GetView<BooksPageController> {
           )
         ],
       ),
-      body: const Center(
-        child: Text(
-          'BooksPageView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      body: ListView(
+        children: ListTile.divideTiles(
+          context: context,
+          tiles: List.generate(2, (index) {
+            return SlideMenu(
+              menuItems: <Widget>[
+                Container(
+                  color: Colors.black12,
+                  child: IconButton(
+                    icon: const Icon(Icons.more_horiz),
+                    onPressed: () {},
+                  ),
+                ),
+                Container(
+                  color: Colors.red,
+                  child: IconButton(
+                    color: Colors.white,
+                    icon: const Icon(Icons.delete),
+                    onPressed: () {},
+                  ),
+                ),
+              ],
+              child: const ListTile(
+                title: Text("Edit Book"),
+              ),
+            );
+          }),
+        ).toList(),
+      ),
+    );
+  }
+}
+
+class SlideMenu extends StatefulWidget {
+  final Widget child;
+  final List<Widget> menuItems;
+
+  const SlideMenu({Key? key, required this.child, required this.menuItems})
+      : super(key: key);
+
+  @override
+  State<SlideMenu> createState() => _SlideMenuState();
+}
+
+class _SlideMenuState extends State<SlideMenu>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  initState() {
+    super.initState();
+    _controller = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 200));
+  }
+
+  @override
+  dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final animation =
+        Tween(begin: const Offset(0.0, 0.0), end: const Offset(-0.2, 0.0))
+            .animate(CurveTween(curve: Curves.decelerate).animate(_controller));
+
+    return GestureDetector(
+      onHorizontalDragUpdate: (data) {
+        setState(() {
+          _controller.value -=
+              (data.primaryDelta! / (context.size!.width * 0.2));
+        });
+      },
+      onHorizontalDragEnd: (data) {
+        if (data.primaryVelocity! > 1500) {
+          _controller.animateTo(.0);
+        } else if (_controller.value >= .5 || data.primaryVelocity! < -1500) {
+          _controller.animateTo(1.0);
+        } else {
+          _controller.animateTo(.0);
+        }
+      },
+      child: LayoutBuilder(
+        builder: (context, constraint) {
+          return Stack(
+            children: [
+              SlideTransition(
+                position: animation,
+                child: widget.child,
+              ),
+              AnimatedBuilder(
+                animation: _controller,
+                builder: (context, child) {
+                  return Positioned(
+                    right: .0,
+                    top: .0,
+                    bottom: .0,
+                    width: constraint.maxWidth * animation.value.dx * -1,
+                    child: Row(
+                      children: widget.menuItems.map((child) {
+                        return Expanded(
+                          child: child,
+                        );
+                      }).toList(),
+                    ),
+                  );
+                },
+              ),
+            ],
+          );
+        },
       ),
     );
   }
