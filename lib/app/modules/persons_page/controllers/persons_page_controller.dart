1f75527bdd3fe82480f25f7ebc9413a7568eb6ad
skpatel9081 <krunals9102001@gmail.com>
2023-02-27 14:32:58 +0530

    ADD :AK-028
    -delete person

--- lib/app/modules/persons_page/controllers/persons_page_controller.dart
+++ lib/app/modules/persons_page/controllers/persons_page_controller.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../../../core/utils/toast.dart';
@@ -6,10 +7,14 @@ import '../../../data/model/response_model/list_of_person_response.dart';
 import '../../../data/repository/auth_repository.dart';
 
 class PersonsPageController extends GetxController {
+  TextEditingController passwordController = TextEditingController();
+
   final _listOfPersonData = PersonListResponse().obs;
   PersonListResponse get listOfPersonData => _listOfPersonData.value;
   set listOfPersonData(PersonListResponse value) =>
       _listOfPersonData.value = value;
+
+  final formGlobalKey = GlobalKey<FormState>();
 
   @override
   void onInit() {
@@ -31,9 +36,16 @@ class PersonsPageController extends GetxController {
 
   Future<void> deletePerson({required String id}) async {
     try {
-      final response = await AuthRepository.deletePerson(personID: id);
-      listOfPersonData = response;
+      final response = await AuthRepository.deletePerson(
+        requestData: {
+          "password": passwordController.text,
+        },
+        personID: id,
+      );
       ToastUtils.showBottomSnackbar("${response.message}");
+      passwordController.clear();
+      listOfPersonData = response;
+      Get.back(closeOverlays: true);
       getPersonList();
     } on DioError catch (e) {
       Get.back();
